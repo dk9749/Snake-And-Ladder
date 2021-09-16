0a53c179c29109f4b3996559f3778ea39ba5187e
Dinesh Kumar <mr.dineshdkd@gmail.com>
2021-09-16 18:37:13 +0530

    Added: Using Random To Check For Options

--- SnakeAndLadderUC1.java
+++ SnakeAndLadderUC1.java
@@ -5,6 +5,21 @@ public class SnakeAndLadderUC1 {
 
     public static void main(String[] args) {
         int die = 0;
-        die = (int) Math.floor(Math.random() % 10 * 6 + 1);
+        int random = 0;
+        do {
+            die = (int) Math.floor(Math.random() % 10 * 6 + 1);
+            random = (int) Math.floor(Math.random() % 10 * 3 + 1);
+
+            if (random == 1) {
+                System.out.println("Player1 Is Now At Position: " + playerPosition);
+
+            } else if (random == 2) {
+                System.out.println("Player1 Is Now At Position: " + playerPosition);
+
+            } else {
+                System.out.println("Player1 Is Now At Position: " + playerPosition);
+
+            }
+        } while (playerPosition != 100);
     }
 }
