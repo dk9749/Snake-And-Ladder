4dd8c0a51ae89779fda6a9abc52885dcbfec857a
Dinesh Kumar <mr.dineshdkd@gmail.com>
2021-09-16 19:00:13 +0530

    Added: If Player Moves Below 0 Then The Player Starts From 0

--- SnakeAndLadderUC1.java
+++ SnakeAndLadderUC1.java
@@ -3,6 +3,15 @@ package com.bridgelabz;
 public class SnakeAndLadderUC1 {
     private static int playerPosition = 0;
 
+    private static int snake(int die) {
+        System.out.println("Player1 is Bitten By Snake");
+        if(playerPosition - die < 1){
+            return 0;
+        }else{
+            return playerPosition - die;
+        }
+    }
+
     public static void main(String[] args) {
         int die = 0;
         int random = 0;
@@ -11,12 +20,14 @@ public class SnakeAndLadderUC1 {
             random = (int) Math.floor(Math.random() % 10 * 3 + 1);
 
             if (random == 1) {
+                System.out.println("Player1 Is Still At The Same Position");
                 System.out.println("Player1 Is Now At Position: " + playerPosition);
 
             } else if (random == 2) {
                 System.out.println("Player1 Is Now At Position: " + playerPosition);
 
             } else {
+                playerPosition =  snake(die);
                 System.out.println("Player1 Is Now At Position: " + playerPosition);
 
             }
