150ed7cf5ba582a3eed89faa289680d4c5533a17
Dinesh Kumar <mr.dineshdkd@gmail.com>
2021-09-16 18:28:30 +0530

    Added: Using Random To Get Die Number

--- SnakeAndLadderUC1.java
+++ SnakeAndLadderUC1.java
@@ -2,4 +2,9 @@ package com.bridgelabz;
 
 public class SnakeAndLadderUC1 {
     private static int playerPosition = 0;
+
+    public static void main(String[] args) {
+        int die = 0;
+        die = (int) Math.floor(Math.random() % 10 * 6 + 1);
+    }
 }
