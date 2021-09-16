053b8ae5eb3b1f784c939eb3a05127abc3d77105
Dinesh Kumar <mr.dineshdkd@gmail.com>
2021-09-16 22:56:44 +0530

    Added: Number Of Dice With Player Position

--- SnakeAndLadderUC1.java
+++ SnakeAndLadderUC1.java
@@ -24,9 +24,11 @@ public class SnakeAndLadderUC1 {
     public static void main(String[] args) {
         int die = 0;
         int random = 0;
+        int numberOfDiceRolled = 0;
         do {
             die = (int) Math.floor(Math.random() % 10 * 6 + 1);
             random = (int) Math.floor(Math.random() % 10 * 3 + 1);
+            numberOfDiceRolled = numberOfDiceRolled + 1;
 
             if (random == 1) {
                 System.out.println("Player1 Is Still At The Same Position");
@@ -42,5 +44,6 @@ public class SnakeAndLadderUC1 {
 
             }
         } while (playerPosition != 100);
+        System.out.println("Number Of Die Rolls: " + numberOfDiceRolled);
     }
 }
